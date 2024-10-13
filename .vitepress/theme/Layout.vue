0fae96acdeb68669134deb798fd3712e87e75a8a
barbapapazes <e.soubiran25@gmail.com>
2024-10-13 09:28:22 +0200

    feat: custom theme

---
+++ .vitepress/theme/Layout.vue
@@ -0,0 +1,15 @@
+<script lang="ts" setup>
+import { useRoute } from 'vitepress'
+
+const route = useRoute()
+
+console.log(route)
+</script>
+
+<template>
+  <div>
+    <h1>Garabit Blog</h1>
+
+    <Content />
+  </div>
+</template>
